96611f855a6dacb08ebb1424eead8bcc10685656
chrisZip <kapitachristian2003@gmail.com>
2021-03-28 00:48:21 -0400

    update MongoDB connection

--- server.js
+++ server.js
@@ -4,22 +4,27 @@ const mongoose=require("mongoose");
 const logger=require("morgan");
 
 const app=express();
-const PORT=process.env.PORT|| 3000;
+const PORT=process.env.PORT|| 8080;
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-const db = require('./app/models');
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+useNewUrleParser: True,
+usedUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false});
 
 require("./app/routes/apiRoutes.js")(app);
 require("./app/routes/htmlRoutes.js")(app);
 
-//Uncomment Line 21 to create demo data
-// require('./seeders/seed.js')(db);
+//Uncomment to seed data to MongoDB
+//require('./seeders/seed');
+
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
